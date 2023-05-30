adb7e7c34a0caae5bc8bd18604088892157a0a06
Mikhail Maslov <42963781+mkmaslov@users.noreply.github.com>
2023-05-30 11:24:39 +0200

    Create Arch Linux installation medium
    Updated the scritp

--- create_USB.sh
+++ create_USB.sh
@@ -1,40 +1,44 @@
-#!/bin/bash
-RED="\033[1;31m"
-GREEN="\033[1;32m"
-BLUE="\033[1;34m"
-WHITE="\033[0m"
-message(){ echo -e "${BLUE}$1${WHITE}"; }
-warning(){ echo -e "${RED}$1${WHITE}"; }
-success(){ echo -e "${GREEN}$1${WHITE}"; }
-question(){ echo -e -n "${BLUE}$1${GREEN}"; read answer; echo -e -n "${WHITE}"; }
-message "This script creates Arch Linux installation medium"
-warning "WARNING: to avoid permission conflicts, this script uses sudo"
-sudo echo -n ""
-sudo mkdir create_USB_temp
-cd create_USB_temp
-message "Downloading latest Arch Linux image and its GnuPG signature"
-sudo wget http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso.sig
-sudo wget http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso
-message "Verifying image signature"
-gpg --keyserver-options auto-key-retrieve --verify archlinux-x86_64.iso.sig archlinux-x86_64.iso
-message "The output above should contain \"Good signature from ...\" line."
-message "Additionally, you need to compare the fingerprint above with the official PGP fingerprint (from https://archlinux.org/download/)."
-question "Do you want to proceed? [y/N]: "
-if [ $answer == "y" ]; then
-	message "List of connected storage devices"
-	sudo lsblk -a -d -o PATH,SIZE
-	warning "WARNING: the chosen drive will be erased!"
-	question "Choose drive [PATH, e.g. /dev/sdX]: "
-	message "Creating installation medium on ${answer}"
-	for partition in $answer?*; do sudo umount -q $partition; done
-	sudo wipefs --all $answer
-	sudo dd bs=4M if=archlinux-x86_64.iso of=$answer conv=fsync oflag=direct status=progress
-	sudo sync
-	success "Installation medium successfully created"
+#!/usr/bin/env -S bash -e
+
+YELLOW="\e[1;33m"
+WHITE="\e[0m"
+# Highlight a message.
+function say () { echo -e "${YELLOW}$1${WHITE}";}
+# Prompt for a response.
+function ask () { 
+    echo -e -n "${YELLOW}$1" && read response && echo -e -n "${WHITE}"
+}
+
+say "Creating Arch Linux installation medium."
+mkdir arch_temp && cd arch_temp
+say "Downloading latest .iso and its GPG signature."
+wget -q --show-progress \
+    http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso.sig \
+    http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso
+say "Verifying image signature."
+gpg --keyserver-options auto-key-retrieve \
+    --verify archlinux-x86_64.iso.sig archlinux-x86_64.iso
+say "The output above should contain the \"Good signature from ...\" line."
+say "Also, you need to compare the fingerprint above with the official PGP \
+fingerprint (from https://archlinux.org/download/)."
+ask "Do you want to proceed [y/N]? "
+if [[ $response =~ ^(yes|y)$ ]]; then
+	lsblk -a -d -o PATH,SIZE
+	ask "Choose the drive: /dev/"
+    DISK="/dev/$response"
+    ask "This will delete all the data on $DISK. Do you agree [y/N]? "
+    if [[ $response =~ ^(yes|y)$ ]]; then
+        for partition in $DISK?*; do umount -q "$partition"; done
+        sudo dd bs=4M if=archlinux-x86_64.iso of="$DISK" conv=fsync oflag=direct status=progress
+	    sync
+        for partition in $DISK?*; do umount -q "$partition"; done
+        say "USB installation medium successfully created."
+    else
+        say "Canceling operation."
+    fi
 else
-	warning "Creation procedure failed"
+    say "Canceling operation."
 fi
 cd ..
-question "Do you want to remove the downloaded iso? [y/N]"
-if [ $answer == "y" ]; then
-	sudo rm -rf create_USB_temp
+rm -rf arch_temp
+exit
