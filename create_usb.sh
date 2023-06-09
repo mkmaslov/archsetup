000f72bf14a6ba30bcefaad775f15290637f084c
Mikhail Maslov <42963781+mkmaslov@users.noreply.github.com>
2023-06-09 23:24:30 +0200

    Create Arch Linux USB installation medium
    --  Fixed umount "not mounted" error.
    --  Shortened code.
    --  Replaced "umount" command after "sync" with an "eject" command.

--- create_usb.sh
+++ create_usb.sh
@@ -1,44 +1,44 @@
-#!/usr/bin/env -S bash -e
+#!/bin/bash
+# Create bootable USB drive using the latest Arch Linux ISO.
+set -e
 
-YELLOW="\e[1;33m"
-WHITE="\e[0m"
 # Highlight a message.
-function say () { echo -e "${YELLOW}$1${WHITE}";}
+color_on="\e[1;33m" && color_off="\e[0m"
+function say () { echo -e "${color_on}$1${color_off}";}
+
 # Prompt for a response.
 function ask () { 
-    echo -e -n "${YELLOW}$1" && read response && echo -e -n "${WHITE}"
+  echo -en "${color_on}$1" && read response && echo -en "${color_off}"
 }
 
 say "Creating Arch Linux installation medium."
 mkdir arch_temp && cd arch_temp
 say "Downloading latest .iso and its GPG signature."
 wget -q --show-progress \
-    http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso.sig \
-    http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso
+  http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso.sig \
+  http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso
 say "Verifying image signature."
-gpg --keyserver-options auto-key-retrieve \
-    --verify archlinux-x86_64.iso.sig archlinux-x86_64.iso
-say "The output above should contain the \"Good signature from ...\" line."
-say "Also, you need to compare the fingerprint above with the official PGP \
+gpg --keyserver-options auto-key-retrieve --verify *.iso.sig *.iso
+say "Output above should contain \"Good signature from ...\". \
+Also, you need to compare the fingerprint with the official PGP \
 fingerprint (from https://archlinux.org/download/)."
 ask "Do you want to proceed [y/N]? "
-if [[ $response =~ ^(yes|y)$ ]]; then
-	lsblk -a -d -o PATH,SIZE
-	ask "Choose the drive: /dev/"
-    DISK="/dev/$response"
-    ask "This will delete all the data on $DISK. Do you agree [y/N]? "
-    if [[ $response =~ ^(yes|y)$ ]]; then
-        for partition in $DISK?*; do umount -q "$partition"; done
-        sudo dd bs=4M if=archlinux-x86_64.iso of="$DISK" conv=fsync oflag=direct status=progress
-	    sync
-        for partition in $DISK?*; do umount -q "$partition"; done
-        say "USB installation medium successfully created."
-    else
-        say "Canceling operation."
-    fi
-else
+if [[ $response =~ ^(yes|y|Y|YES|Yes)$ ]]; then
+  lsblk -ado PATH,SIZE
+  ask "Choose the drive: /dev/" && disk="/dev/$response"
+  ask "This will delete all the data on $disk. Do you agree [y/N]? "
+  if [[ $response =~ ^(yes|y|Y|YES|Yes)$ ]]; then
+    # Return "true", if umount throws "not mounted" error
+    umount -q $disk?* || /bin/true
+    sudo dd bs=4M if=archlinux-x86_64.iso of=$disk \
+      conv=fsync oflag=direct status=progress
+    sudo sync && sudo eject $disk
+    say "USB installation medium successfully created."
+  else
     say "Canceling operation."
+  fi
+else
+  say "Canceling operation."
 fi
-cd ..
-rm -rf arch_temp
+cd .. && rm -rf arch_temp
 exit
