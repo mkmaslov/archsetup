62fbeee604966234760a1c9557d87285e7b76836
Mikhail Maslov <42963781+mkmaslov@users.noreply.github.com>
2023-06-10 00:11:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@
 set -e
 
 # Highlight a message.
-color_on="\e[1;33m" && color_off="\e[0m"
-function say () { echo -e "${color_on}$1${color_off}";}
+yellow="\e[1;33m" && red="\e[1;31m" && color_off="\e[0m"
+function say () { echo -e "${yellow}$1${color_off}";}
+function error () { echo -e "${red}$1${color_off}";}
 
 # Prompt for a response.
 function ask () { 
-  echo -en "${color_on}$1" && read response && echo -en "${color_off}"
+  echo -en "${yellow}$1" && read response && echo -en "${color_off}"
 }
 
 # Confirm continuing evaluation.
@@ -19,10 +20,18 @@ function confirm () {
     fi
 }
 
-# Reset terminal
+# Reset terminal.
 loadkeys us && setfont ter-132b && clear
 say "\n** Arch Linux Installation **\n"
-say "Before proceeding, please activate your WiFi connection."
-say "For instance, use: iwctl station wlan0 connect <ESSID>."
-say "To probe connection to Internet, use: ping archlinux.org."
-confirm "Do you confirm that the Internet is reachable?"
+
+# Test Internet connection.
+say "Testing Internet connection."
+ping -w 5 archlinux.org &>/dev/null
+further=$?
+if [ $further -ne 0 ]; then
+    error "Couldn't connect to Internet."
+    echo -e "Before proceeding with the installation, you must have a functioning Internet connection."
+    echo -e "To connect to WiFi network, use: iwctl station wlan0 connect <ESSID>."
+    echo -e "To test your connection, use: ping archlinux.org."
+    exit
+fi
