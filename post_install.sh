db8e563cba527dde234306fd8344735f0e037a57
mkmaslov <42963781+mkmaslov@users.noreply.github.com>
2023-06-28 15:21:17 +0200

    Added post-install script

---
+++ post_install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# This script installs and configures applications.
+# It should be run on an already booted fresh Arch Linux installation.
+# This script:
+# --  configures zsh shell
+# --  configures nvim text editor
+# --  sets up an isolated python environment with Jupyter Notebook 
+
+RES="https://raw.githubusercontent.com/mkmaslov/archsetup/main/resources"
+
+# Configure zsh shell.
+curl "${RES}/.zshrc" > "${HOME}/.zshrc"
+
+# Configure nvim text editor.
+curl "${RES}/.vimrc" > "${HOME}/.vimrc"
+mkdir -p ${HOME}/.config/nvim
+curl "${RES}/init.vim" > "${HOME}/.config/nvim/init.vim" 
+curl -fLo ${HOME}/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +'PlugInstall --sync' +qa
+
+# Setting up virtual environment for Python
+PYDIR="${HOME}/.python_venv/"
+PIP="${PYDIR}/bin/pip"
+JUPYTER="${PYDIR}/bin/jupyter"
+mkdir ${PYDIR} && mkdir ${PYDIR}/cache
+cat > ${PYDIR}/pip.conf <<EOF
+[global]
+cache-dir=${PYDIR}/cache
+EOF
+${PIP} install --upgrade pip --require-virtualenv
+${PIP} install numpy scipy sympy matplotlib notebook\
+  jupyter_contrib_nbextensions jupyter_nbextensions_configurator\
+  --require-virtualenv
+${JUPYTER} contrib nbextension install --sys-prefix
+${JUPYTER} nbextensions_configurator enable --sys-prefix
+
+# To-do: set up environmental variables in this script.
---
+++ resources/.vimrc
@@ -0,0 +1,14 @@
+syntax on
+set number 
+set ruler 
+set expandtab
+set ai
+set hlsearch
+set tabstop=4
+set shiftwidth=2
+set clipboard+=unnamedplus
+set colorcolumn=80
+highlight ColorColumn ctermbg=0 guibg=lightgrey
+call plug#begin()
+Plug 'scrooloose/nerdtree'
+call plug#end()
