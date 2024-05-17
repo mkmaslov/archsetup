765babe731bc1db09507af46a7da44412731230c
mkmaslov <42963781+mkmaslov@users.noreply.github.com>
2024-05-17 17:17:09 +0200

    refined USB creation script

--- create_usb.sh
+++ create_usb.sh
@@ -2,66 +2,76 @@
 
 set -e
 
+# -----------------------------------------------------------------------------
 # This script creates bootable USB drive using the latest Arch Linux image.
+# -----------------------------------------------------------------------------
+
+# Output formatting.
 
 # Highlight the output.
-YELLOW="\e[1;33m" && COLOR_OFF="\e[0m"
-cprint() { echo -ne "${YELLOW}${1}${COLOR_OFF}"; }
-msg() { cprint "${1}\n"; }
+YELLOW="\e[1;33m" ; RED="\e[1;31m" ; COLOR_OFF="\e[0m"
+cprint() { echo -ne "${1}${2}${COLOR_OFF}" ; }
+msg() { cprint "${YELLOW}" "${1}\n" ; }
+error() { cprint "${RED}" "ERROR: ${1}\n" ; }
 
 # Prompt for a response.
-ask () { cprint "${1} " && echo -ne "$2" && read RESPONSE; }
+ask () { cprint "${YELLOW}" "${1} " ; echo -ne "${2}" ; read RESPONSE ; }
 
+# -----------------------------------------------------------------------------
 
+msg "Creating Arch Linux USB installation medium."
 
-# Create cache directory.
-msg "Creating Arch Linux installation medium."
-# Clear cache if exists.
+# Clear cache directory if exists.
 rm -rf archinstall_cache &> /dev/null
+# Create cache directory.
 mkdir archinstall_cache && cd archinstall_cache
 
-# Download image and its GPG signature.
+# Download Arch Linux image and its GPG signature.
 msg "Downloading the latest Arch Linux image and its GPG signature:"
-if ! [ -x "$(command -v wget)" ]; then
-  if ! [ -x "$(command -v curl)" ]; then
-    msg "ERROR: please install either 'wget' or 'curl' to be able to proceed."
-    exit
+# kurl() function copied from: https://stackoverflow.com/a/66504482
+kurl(){ F=${1##*/} ; printf "%32s[%s]" "" "${F}" ; COLUMNS=28 curl -# ${1} -o ${F}; }
+# Check whether wget or curl are available, if not - return error.
+download() {
+  if [ -x "$(command -v wget)" ]; then
+    wget -q --show-progress "${1}" "${2}"
+  elif [ -x "$(command -v curl)" ]; then
+    kurl "${1}" && kurl "${2}"
   else
-    curl --progress-bar \
-      -O http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso.sig \
-      -O http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso
+    error "please install either \"wget\" or \"curl\" to proceed."
+    exit
   fi
-else
-  wget -q --show-progress \
-    http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso.sig \
-    http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso
-fi
+}
+IMAGE="http://mirror.easyname.at/archlinux/iso/latest/archlinux-x86_64.iso"
+download "${IMAGE}.sig" "${IMAGE}"
 
-# Verify the signature.
-msg "\nVerifying image signature:"
+# Verify image signature.
+msg "Verifying image signature:"
 gpg --keyserver-options auto-key-retrieve --verify *.iso.sig *.iso
-msg "Output above should contain \"Good signature from ...\". \
-Also, you need to compare the fingerprint with the official PGP \
-fingerprint (from https://archlinux.org/download/)."
-ask "Do you want to proceed [y/N]?"
+msg "Output above should contain \"Good signature from ...\" line."
+msg "The above fingerprint should match this fingerprint:"
+msg "(from https://archlinux.org/download/)"
+PGPKEY=$(curl --silent https://archlinux.org/download/ | \
+  grep -o "title=\"PGP key search.*\"" | cut -d " " -f 5-14)
+echo "Primary key fingerprint: ${PGPKEY::-1}"
+ask "Do you confirm that the GPG signature is correct [y/N]?"
 
 if [[ $RESPONSE =~ ^(yes|y|Y|YES|Yes)$ ]]; then
   # Scan hardware for storage devices.
-  msg "\nAvailable storage devices:"
-  lsblk -ao PATH,SIZE,TYPE,MOUNTPOINTS
-  ask "Choose the drive (with TYPE==disk):" "/dev/" && disk="/dev/$RESPONSE"
-  ask "This will delete all the data on $disk. Do you agree [y/N]?"
+  msg "Available storage devices:"
+  lsblk -ao PATH,SIZE,TYPE,MOUNTPOINTS | grep -E "TYPE|disk"
+  ask "Select the USB drive:" "/dev/" && DISK="/dev/$RESPONSE"
+  ask "Proceeding will erase all data on ${DISK}. Do you agree [y/N]?"
   if [[ $RESPONSE =~ ^(yes|y|Y|YES|Yes)$ ]]; then
     # Return "true", if umount throws "not mounted" error.
-    msg "\nWriting to disks requires superuser access:"
-    umount -q $disk?* || /bin/true && sudo wipefs --all $disk
+    msg "Writing to disks requires superuser access:"
+    umount -q ${DISK}?* || /bin/true && sudo wipefs --all ${DISK} > /dev/null
     # Write image into USB disk.
-    msg "Writing the image. Do NOT remove the drive."
-    sudo dd bs=4M if=archlinux-x86_64.iso of=$disk \
-      conv=fsync oflag=direct status=progress
-    # Check that all data is transferred and remove the drive.
-    sudo sync && sudo eject $disk
-    msg "Installation medium successfully created. You can remove the drive."
+    msg "Writing Arch Linux image to the USB drive. Do NOT remove the drive."
+    sudo dd bs=4M if=archlinux-x86_64.iso \
+      of=${DISK} conv=fsync oflag=direct status=progress
+    # Check that all data is transferred and eject the drive.
+    sudo sync && sudo eject ${DISK}
+    msg "USB installation medium created. You can remove the USB drive."
   else
     msg "Canceling operation."
   fi
@@ -69,6 +79,6 @@ else
   msg "Canceling operation."
 fi
 
-# Remove temporary directory.
+# Remove cache directory.
 cd .. && rm -rf archinstall_cache
 exit
